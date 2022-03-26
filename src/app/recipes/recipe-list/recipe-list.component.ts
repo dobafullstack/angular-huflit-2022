ae85304b6d4c506d7f8cfaa9542822f76ae5948a
duyanhitbe <duyanh.it.work@gmail.com>
2022-03-26 15:57:01 +0700

    week_2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import Recipe from 'src/app/models/Recipe';
 
 @Component({
@@ -7,6 +7,8 @@ import Recipe from 'src/app/models/Recipe';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe({
       name: 'Pizza',
@@ -14,7 +16,17 @@ export class RecipeListComponent implements OnInit {
       imagePath:
         'https://cdn.tgdd.vn/2020/09/CookProduct/1200bzhspm-1200x676.jpg',
     }),
+    new Recipe({
+      name: 'Hamburger',
+      description: 'America',
+      imagePath:
+        'https://cdn.tgdd.vn/2020/09/CookProduct/1200bzhspm-1200x676.jpg',
+    }),
   ];
+
+  onSelect(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
 
   constructor() {}
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Recipe from '../models/Recipe';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  recipe: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(recipe: Recipe){
+    this.recipe = recipe;
+  }
 }
